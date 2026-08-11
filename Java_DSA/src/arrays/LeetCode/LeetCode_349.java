e32a5107addf64701a52dcd50197f65ba7f68694
krish pachori <krishpachori18@gmail.com>
2026-08-11 14:50:31 +0530

    Added LeetCode 349 solution

---
+++ Java_DSA/src/arrays/LeetCode/LeetCode_349.java
@@ -0,0 +1,28 @@
+package arrays.LeetCode;
+
+class LeetCode_349 {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        boolean[] seen = new boolean[1001];
+
+        for (int num : nums1) {
+            seen[num] = true;
+        }
+
+        boolean[] add = new boolean[1001];
+
+        Set<Integer> resSet = new HashSet<>();
+        for (int num : nums2) {
+            if (seen[num] == true) {
+                resSet.add(num);
+            }
+        }
+
+        int[] res = new int[resSet.size()];
+        int i = 0;
+
+        for (int num : resSet) {
+            res[i++] = num;
+        }
+        return res;
+    }
+}
